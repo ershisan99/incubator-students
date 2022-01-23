ecf82243e5f0329906af00c92af0a2b535263a0c
Andres <zadorozhnyi99.dev@gmail.com>
2022-01-23 19:51:23 +0100

    add queries

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,15 @@ import { gql } from "@apollo/client";
 import client from "../apollo-client";
 
 export const Home = () => {
-	return (
-		<div className="flex flex-col items-center justify-center min-h-screen py-2 ">
-			<Head>
-				<title>Student's page</title>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-		</div>
-	);
+   return (
+      <div className="flex flex-col items-center justify-center py-2 ">
+         <Head>
+            <title>Student's page</title>
+            <link rel="icon" href="/favicon.ico" />
+         </Head>
+         <main className="text-white">Some stats and whatnot</main>
+      </div>
+   );
 };
 
 export default Home;
